85cb05e671b700b1ece06d77dd9ef2a243e04508
Pierpaolo Papero <f.comitani@ucl.ac.uk>
2017-12-13 23:59:22 +0000

    Upload Template

--- README.rst
+++ README.rst
@@ -1,7 +1,12 @@
-# TS-PPTIS
+TS-PPTIS (Transition State PPTIS)
+=================================
+
+Version 2.0
+-----------
 
 New implementation of TS-PPTIS for Gromacs 5.X and Plumed 2.X
 Based on scripts by G. Saladino and J. Juraszek @2014-2016 
 
-For Reference, see 
+For reference see 
+
 Juraszek J, Saladino G, van Erp TS, and Gervasio FL, "Efficient numerical reconstruction of protein folding kinetics with partial path sampling and pathlike variables." Phys Rev Lett. 2013 Mar 8;110(10):108106.
---
+++ TS-PPTIS/__init__.py
@@ -0,0 +1,39 @@
+"""
+TS-PPTIS v2.0
+F. Comitani and G. Mattedi @2017 
+
+New implementation of TS-PPTIS for Gromacs 5.X and Plumed 2.X
+Based on scripts by G. Saladino and J. Juraszek @2014-2016
+
+For reference see
+
+Juraszek J, Saladino G, van Erp TS, and Gervasio FL, "Efficient numerical reconstruction of protein folding kinetics with partial path sampling and pathlike variables." Phys Rev Lett. 2013 Mar 8;110(10):108106. 
+
+"""
+
+
+import sys
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+import mdtraj as md
+
+
+class ts:
+	""" Standard TS-PPTIS setup and analysis class. """
+
+	def __init__(self):
+
+		"""Initialise TS-PPTIS.
+		Args:
+			[...]
+				
+		"""
+		self.filler=True
+
+
+
+if __name__ == "__main__":
+
+	return
