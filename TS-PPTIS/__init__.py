81cb00d20767944d4ba3d5d0dbaa82bde5ee40b8
Pierpaolo Papero <f.comitani@ucl.ac.uk>
2017-12-14 23:26:21 +0000

    Add first input file checks

--- TS-PPTIS/__init__.py
+++ TS-PPTIS/__init__.py
@@ -1,6 +1,5 @@
 """
 TS-PPTIS v2.0
-F. Comitani and G. Mattedi @2017 
 
 New implementation of TS-PPTIS for Gromacs 5.X and Plumed 2.X
 Based on scripts by G. Saladino and J. Juraszek @2014-2016
@@ -9,31 +8,81 @@ For reference see
 
 Juraszek J, Saladino G, van Erp TS, and Gervasio FL, "Efficient numerical reconstruction of protein folding kinetics with partial path sampling and pathlike variables." Phys Rev Lett. 2013 Mar 8;110(10):108106. 
 
+F. Comitani: f.comitani@ucl.ac.uk
+G. Mattedi: giulio.mattedi.16@ucl.ac.uk
+@2017-2018 
+
 """
 
 
-import sys
+import sys, os, subprocess
 import numpy as np
 
 import matplotlib.pyplot as plt
 
 import mdtraj as md
 
+from tools import *
 
-class ts:
-	""" Standard TS-PPTIS setup and analysis class. """
+class tsSetup:
+	""" Standard TS-PPTIS setup class. """
 
-	def __init__(self):
+	def __init__(self, top, gro, traj, windows, ndx='', gmx='$GMX'):
 
 		"""Initialise TS-PPTIS.
 		Args:
-			[...]
-				
-		"""
-		self.filler=True
+			top (string): path to topology file .top
+			gro (string): path to structure file .gro
+			traj (string): path to initial trajectory .trr/.xtc
+			windows (string): path to text file containing information 
+				on the windows in the format left:center:right
+			ndx (string, optional): path to groups definition file .ndx
+			gmx (string, optional): path to the local gromacs executable.
+
+	 	"""
+
+		"""Check and load trajectory data."""
+		try:
+			self.trajData=md.load(traj, top=top)
+			print 'Topology and trajectory files:\tOK'
+		except:
+			sys.exit('Error: invalid input topology/trajectory files ' + top + ' ' + traj)
+		
+		"""Check and load windows file."""	
+		try:
+			self.winList=parseWindows(windows)
+			print 'PPTIS windows:\t\t\tOK'
+		except:
+			sys.exit('Error: invalid windows file ' + windows)
+		
+		"""Check ndx file."""
+		if os.path.isfile(ndx): 
+			self.ndx=ndx
+		else:
+			self.ndx=''
+			print 'nxd file:\t\t\tnot found' 	
 
 
+		"""Check local gromacs installation."""
+		self.gmx=findExe(gmx)
+		if self.gmx!=None:
+			print 'Gromacs installation:\t\tOK'
+	 	else:
+			sys.exit('Error : invalid gmx path ' + gmx+'\n'+\
+				'Make sure to have a working version of gromacs 5.X installed!')
+
+			
+
+def testAll():
+	
+	ts=tsSetup('../testfiles/topology.top',
+			'../testfiles/structure.gro',
+			'../testfiles/traj.xtc',
+			'../testfiles/windows.dat',
+			gmx='gmx')
 
 if __name__ == "__main__":
 
-	return
+	print("Running test...\n")
+	testAll()
+	print("\nDone!")
---
+++ TS-PPTIS/tools.py
@@ -0,0 +1,93 @@
+"""
+TS-PPTIS tools
+
+A set of utility functions for TS-PPTIS
+
+F. Comitani: f.comitani@ucl.ac.uk
+G. Mattedi: giulio.mattedi.16@ucl.ac.uk
+@2017-2018 
+
+"""
+
+
+import sys,os,subprocess
+import numpy as np
+
+
+def parseWindows(nameFile):
+
+        """Parse a text file containing information on the TS-PPTIS windows.
+
+        Args:
+                  windows (string): path to  text file containing information 
+	                  on the windows in the format left:center:right
+
+	Returns:
+		  outList (nested list, float): list containing left, center and right value
+	 		  of each window.
+
+               """
+
+	outList = []
+	data =open(nameFile,"r")
+
+	for line in data.readlines():
+		outList.append([])
+		for i in line.split(':'):
+			outList[-1].append(float(i))
+	data.close()
+
+	return outList
+
+
+def isExe(path):
+
+	"""Check if program exists and is executable.
+
+	Args:
+		path (string): path to the program.
+
+	Returns:
+		(bool): True or False if the program is executable.
+
+	"""
+        return os.path.isfile(path) and os.access(path, os.X_OK)
+
+def findExe(fileName):
+
+	"""Finds the full path of a program and check if it is executable.
+
+	Args:
+		fileName (string): name of the program or path pointing to it.
+
+	Returns:
+		(string): full path to the program if it exists..
+
+	"""
+
+	"""In case it's an alias."""
+	if fileName.startswith('$'):
+                p=subprocess.Popen('which '+fileName,shell=True,stdout=subprocess.PIPE)
+		fileName=p.communicate()[0][:-1]
+	
+	fpath, fname = os.path.split(fileName)
+
+	if fpath:
+		if isExe(fileName): return fileName
+	else:
+		for path in os.environ["PATH"].split(os.pathsep):
+			fullPath = os.path.join(path, fileName)
+			if isExe(fullPath):
+				return fullPath
+
+	return None
+
+
+
+
+
+
+if __name__ == "__main__":
+
+	pass
+
