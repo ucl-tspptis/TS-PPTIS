77f4c6a272f1be90940e0219f19cae1d8d81a0c0
fcomitani <f.comitani@ucl.ac.uk>
2018-02-03 00:06:44 +0000

    fix crossings analysis

--- TS-PPTIS/__init__.py
+++ TS-PPTIS/__init__.py
@@ -577,60 +577,65 @@ class tsAnalysis:
         self.beta = 1 / 2.479
         self.crossInfo=[]
     
-    def getCrossings(self, folderName='pptis00', rejectedName='pptis00/tps_rej.dat'):
+    def getCrossings(self, folderName='../testfiles/' ):
         """Reads par files produced by ts-pptis and extracts information on crossing events to be used by getRates.
 
         Args:
 
             ...
             
-            folderName (string, optional): path to the pptis output files #at the moment for one window only
-            rejectedName (string, optional): path to the tps_rej.dat file
+            folderName (string, optional): path to the directory containing the pptis 
+                windows subdirectories
         
         """
 
         # FC: for the moment being, this function reads par files as in Giorgio's implementation
         # it will need to be adapted to read from our new output format.
-        # another issue is that it works for one file at a time, I'm not sure how the crossings for each window
-        # can be put together. 
+        # another issue is that it appends the data from all windows to one single file
+        # (self.crossInfo), I'm not sure how to manage this. 
 
         # WARNING: UNTESTED
 
+        listFold=[]
+        for fold in [x[0] for x in os.walk(folderName)]:
+	    if fold.startswth('pptis'):
+                listFold.append(fold)
 
+        for window in listFold:
         ###find the value of lambda0, for the moment old plumed format, alternatively we can implement it as input
-        plumed=open(folderName+'/plumed.dat',"r")
-        for line in plumed.readlines():
-            if line[0]!='TIS':
-                target=np.float(line[-1])
-                break
-        plumed.close()	   
+            plumed=open(folderName+window+'/plumed.dat',"r")
+            for line in plumed.readlines():
+                if line[0]!='TIS':
+                    target=np.float(line[-1])
+                    break
+            plumed.close()	   
 
-        pp,pm = 0,0
-        velSum, weightsSum = 0,0 #needed only for logging if we decide to keep it
+            pp,pm = 0,0
+            velSum, weightsSum = 0,0 #needed only for logging if we decide to keep it
 
-        #load the par files, to be adapted 
-        listPar=[]
-        for fi in os.listdir(folderName+'/tps_data/'):
-            if fi.endswith(".par"):
-                listPar.append(fi)
-        listSorted=sorted(listPar, key=natural_keys)
+            #load the par files, to be adapted 
+            listPar=[]
+            for fi in os.listdir(folderName+window++'/tps_data/'):
+                if fi.endswith(".par"):
+                    listPar.append(fi)
+            listSorted=sorted(listPar, key=natural_keys)
 
-        #the output will be saved in a dictionary, but we can keep this as a logfile
-        output=open(folderName+'/crossings.dat', 'w')                
-        for fi in listSorted:
-            crossData = analyzeCross(folderName+'/tps_data/'+fi, target) #see tools
-            pp += crossData['p0p']
-            pm += crossData['p0m']  
-            weight = getWeightTraj(rejectedName, fi[:4]) #see tools
-            velSum += crossData['vel']*weight
-            weightsSum += weight
+            #the output will be saved in a dictionary, but we can keep this as a logfile
+            output=open(folderName+window+'/crossings.dat', 'w')                
+            for fi in listSorted:
+                crossData = analyzeCross(folderName+window+'/tps_data/'+fi, target) #see tools
+                pp += crossData['p0p']
+                pm += crossData['p0m']  
+                weight = getWeightTraj(folderName+window+'/tps_rej.log', fi[:4]) #see tools
+                velSum += crossData['vel']*weight
+                weightsSum += weight
         
-        #LOGGING and OUTPUTTING...                
-        if crossData['vel'] > 0:    #but why only positive vel? 
-            output.write('{:s}'.format(fi[:4]) +'\t'+ '{:.4f}'.format(crossData['vel'])+'\t' + '{:d}'.format(crossData['nrPos']) + '\t' +\
+            #LOGGING and OUTPUTTING...                
+            if crossData['vel'] > 0:    #but why only positive vel? 
+                output.write('{:s}'.format(fi[:4]) +'\t'+ '{:.4f}'.format(crossData['vel'])+'\t' + '{:d}'.format(crossData['nrPos']) + '\t' +\
                     '{:d}'.format(crossData['nrNeg']) + '\t' +'{:d}'.format(weight)+ '\t' +crossData['end'] +'\n')  
-            self.crossInfo.append([fi[:4],crossData['vel'],crossData['nrPos'],crossData['nrNeg'],weight,crossData['end']]) #a bit redundant at the moment
-        output.close()
+                self.crossInfo.append([fi[:4],crossData['vel'],crossData['nrPos'],crossData['nrNeg'],weight,crossData['end']]) #a bit redundant at the moment
+            output.close()
 
     
     def getRates(self, fes, Astate=-1, Bstate=-1, Acorr=0, Bcorr=0, indexTS=None, error=None, ratesFile='rates.dat', printFile=False):
@@ -743,13 +748,13 @@ def testAll():
 #                 gmx='/usr/bin/gmx')
                   gmx='/usr/local/gromacs/bin/gmx')
 
-    ts.initWindow('../testfiles/pptis10',
-                  [0.55,1,1.25],
-                  '../testfiles/traj_fixed.xtc',
-                  '../testfiles/COLVAR',
-                  overwrite=True)
+#    ts.initWindow('../testfiles/pptis10',
+#                  [0.55,1,1.25],
+#                  '../testfiles/traj_fixed.xtc',
+#                  '../testfiles/COLVAR',
+#                  overwrite=True)
 
-    ts.setUpTPS('../testfiles/pptis10')
+#    ts.setUpTPS('../testfiles/pptis10')
 
     ts.finalizeTPS('../testfiles/pptis10')
 
