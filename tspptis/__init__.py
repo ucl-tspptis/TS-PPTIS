Fix rate calculation

tspptis/__init__.py
@@ -747,11 +747,17 @@ class tsAnalysis:
                 and not any(s in fold for s in ['/data','/run','/temp']): #easy to break, find alternative
                     listFold.append(fold)
 
+        lambdas=[]
+        for window in listFold:   #redundant loop, find a better way
+            lambdas.append(getLambda(window))
+        lamIdx=getClosestLambda(tsLambda,lambdas)
+
+            
         for window in listFold:
             target=getLambda(window)
-            
-            #keep going only if lambda is at the TS
-            if target!=tsLambda: continue
+
+            #keep going only if lambda is at the TS (or close to it)
+            if target!=lambdas[lamIdx]: continue
 
             pp,pm = 0,0
             velSum, weightsSum = 0,0 #needed only for logging if we decide to keep it
@@ -782,7 +788,7 @@ class tsAnalysis:
             break
 
     
-    def getRates(self, fes, Astate=-1, Bstate=-1, Acorr=0, Bcorr=0, indexTS=None, error=None, printFile=False):
+    def getRates(self, fes, Astate=-1, Bstate=-1, Acorr=0, Bcorr=0, valTS=None, error=None, printFile=False):
         """Reads the free energy surface FES, TS-PPTIS crossing probabilities
         and ouputs, calculate the rate constants and print(them to screen and/or to file.
 
@@ -799,7 +805,7 @@ class tsAnalysis:
                 (e.g. from external potentials)
             Bstate (int, optional): free energy correction to the B state
                 (e.g. from external potentials)
-            indexTS (int, optional): index of the TS in the FES vector provided,
+            valTS (int, optional): value of the TS in the FES vector provided,
                 if none provided automatically look for the highest point in the FES
             error (float, optional): free energy error to calculate the rates range,
                 if none provided automatically assume 1 kT
@@ -813,16 +819,14 @@ class tsAnalysis:
             As = Astate
 
         if Bstate == -1:
-            Bs = len(fes)-1
+            Bs = len(fes[1])-1
         else:
             Bs = Bstate
-        
-        if indexTS == None or indexTS > np.max(fes[0]) or indexTS < np.min(fes[0]):
+        if valTS == None or valTS > np.max(fes[0]) or valTS < np.min(fes[0]):
             iTS = np.argmax(fes[1])
         else:
-            iTS = indexTS
-        # TS=np.argmax(val[:int(len(val)/4)])
-
+            iTS = min(range(len(fes[0])), key=lambda i: abs(fes[0][i]-valTS))
+     
         if error == None:
             error = 1 / self.beta
 
@@ -959,6 +963,7 @@ class tsAnalysis:
 
             else:
                 hist,midBins = np.array([]), np.array([])
+                velEnsemble=[]
 
             # Zip it nicely. Discard original histogram bins h[1]
             globHist.append(zip(midBins,[h[0] for h in hist]))
@@ -996,13 +1001,16 @@ def testAll():
     #plt.show()
 
 
-    #tsa = tsAnalysis('/home/federico/Giulio/pptis_test')     
+    tsa = tsAnalysis('/home/federico/Giulio/pptis_test')     
 
-    #tsa.getProbabilities()
-    #tsa.getCrossings(0.5) 
+    tsa.getProbabilities()
+    tsa.getCrossings(1.25) 
 
-    #plumed2List('/home/federico/Giulio/pptis_test/fes.dat'):
-    #tsa.getRates(fesList) 
+    fesList=plumed2List('/home/federico/Giulio/pptis_test/fes.dat')
+
+    tsa.getRates(fesList,valTS=1.25) 
+
+    a,b= tsa.endPointVel()
 
 if __name__ == "__main__":
 

tspptis/tools.py
@@ -716,13 +716,8 @@ def calcR(posTS, crossInfo, probInfo, debug=False):
     p0p=pcw_ends * 1.0 / pcw
     p0n=ncw_ends * 1.0 / ncw
 
-    diffL=99999
-    lambda0=0
-    for i in range(len(lambdas)):
-        if np.abs(posTS - lambdas[i]) < diffL:
-            diffL=np.abs(posTS - lambdas[i])
-            lambda0=i
-
+    lambda0=getClosestLambda(posTS,lambdas)
+   
     A=[1, ppm[lambda0 + 1]]
     Ab=[1, pmp[lambda0 - 1]]
     R=[]
@@ -735,8 +730,10 @@ def calcR(posTS, crossInfo, probInfo, debug=False):
         AiNom=pmm[lambda0 + m] * ppm[lambda0 + m + 1] * A[m] * A[m + 1]
         AiDen=(pmp[lambda0 + m] * pmm[lambda0 + m + 1] + pmm[lambda0 + m] * ppm[lambda0 + m + 1])\
             * A[m] - pmp[lambda0 + m] * pmm[lambda0 + m + 1] * A[m + 1]
-        if AiDen == 0:
+        if AiDen == 0 or np.isinf(AiNom):
             break
+        if np.isnan(AiNom) or np.isnan(AiDen): #clean this mess...
+           A.append(A[-1])
         if lambda0+m+1 <= len(ppm)-1:
             A.append(AiNom/AiDen)
         else:
@@ -745,12 +742,16 @@ def calcR(posTS, crossInfo, probInfo, debug=False):
         AbiNom=ppp[lambda0 - m] * pmp[lambda0 - m - 1] * Ab[m] * Ab[m + 1]
         AbiDen=(ppm[lambda0 - m] * ppp[lambda0 - m - 1] + ppp[lambda0 - m] * pmp[lambda0 - m - 1])\
             * Ab[m] - ppm[lambda0 - m] * ppp[lambda0 - m - 1] * Ab[m + 1]
-        if AbiDen == 0:
+        if AbiDen == 0 or np.isinf(AbiNom):
+            Ab.append(Ab[-1]) # to ensure they are the same length
             break
+        if np.isnan(AbiNom) or np.isnan(AbiDen):
+            Ab.append(Ab[-1])
         if lambda0-m-1 >= 0:
             Ab.append(AbiNom/AbiDen)
         else:
             Ab.append(Ab[-1])
+    
 
     for m in range(0, len(A)):
         RiNom=0.5 * Rtst * \
@@ -768,10 +769,10 @@ def plumed2List(fileName):
     and convert it to a format readable by getRates.
 
     Args:
-         fileName: path to Plumed 2 fes.dat file.
+         fileNam (string)e: path to Plumed 2 free energy file.
 
     Returns:
-         nested list: fes information in the appropriate format.
+         fesList (nested list, float): fes information in the appropriate format.
     """
 
     fesList=[]
@@ -785,6 +786,26 @@ def plumed2List(fileName):
 
     return fesList
 
+def getClosestLambda(posTS,lambdas):
+    """Find the closest matching transition state lambda value from a list.
+
+    Args:
+         posTS (float): position of the transition state.
+         lambdas (list, float): list of all available windows positions.
+
+    Returns:
+         lambda 0 (float): the closest matching TS lambda.
+    """
+
+
+    diffL=99999
+    lambda0=0
+    for i in range(len(lambdas)):
+        if np.abs(posTS - lambdas[i]) < diffL:
+            diffL=np.abs(posTS - lambdas[i])
+            lambda0=i
+
+    return lambda0
 
 ##################################################################################
 